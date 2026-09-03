f4318d1def254a0a89628bdbe914e2dd2924c0be
ashishxdev <ashishrana2004@gmail.com>
2026-09-03 23:47:53 +0530

    added Zustand cart store

---
+++ app/store/cartStore.js
@@ -0,0 +1,54 @@
+import { create } from "zustand";
+
+export const useCartStore = create((set, get) => ({
+    cart: [],
+
+    addToCart: (product) => {
+        const existing = get().cart.find((item) => item.id === product.id);
+
+        if (existing) {
+            set({
+                cart: get().cart.map((item) => {
+                    item.id === product.id
+                        ? { ...item, quantity: item.quantity + 1 }
+                        : item
+                })
+            })
+        } else {
+            set({
+                cart: [...get().cart, { ...product, quantity: 1 }]
+            })
+        }
+    },
+
+    removeFromCart: (productId) => {
+        set({
+            cart: get().cart.filter((item) => item.id !== productId)
+        })
+    },
+
+    increaseQuantity: (productId) => {
+        set({
+            cart: get().cart.map((item) => item.id === productId
+                ? { ...item, quantity: item.quantity + 1 }
+                : item)
+        })
+    },
+
+    decreaseQuantity: (productId) => {
+        const item = get().cart.find((item) => item.id === productId);
+
+        if (item && item.quantity <= 1) {
+            set({
+                cart: get().cart.filter((item) => item.id !== productId)
+            })
+        } else {
+            set({
+                cart: get().cart.map((item) => item.id === productId
+                    ? { ...item, quantity: item.quantity - 1 }
+                    : item
+                )
+            })
+        }
+    }
+}))
